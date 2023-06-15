0cea830380197ba6746c0e1cdd9054dd5b509208
meseret <mekuriyawmeseret09@gmail.com>
2023-06-15 10:37:40 -0700

    0-run

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Singly linked list
+ * @i: integer
+ * @next: Points to the next node
+ *
+ * Description: Singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int i;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int i);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
